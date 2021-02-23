4a91719a8431ac95c56c4f0f2dab0ab4a0682c57
aliahaleem8 <>
2021-02-23 09:45:10 -0500

    rm notes

--- src/main/java/com/wasiya/will/WillApplication.java
+++ src/main/java/com/wasiya/will/WillApplication.java
@@ -2,7 +2,7 @@ package com.wasiya.will;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//not recognized
+
 @SpringBootApplication
 public class WillApplication {
 
